09d3aa39cbefce3db752d9df07374b0d2313c575
iannisdezwart <iannis__@hotmail.com>
2020-07-15 20:08:04 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,174 @@
+var $ = function (query) { return document.querySelector(query); };
+// Get Elements
+var redSlider = $('#red');
+var greenSlider = $('#green');
+var blueSlider = $('#blue');
+var rgbInput = $('#rgb');
+var hexInput = $('#hex');
+var background = document.body;
+var colourBox = $('#colour-box');
+var Colour = /** @class */ (function () {
+    function Colour(red, green, blue) {
+        if (red === void 0) { red = 0; }
+        if (green === void 0) { green = 0; }
+        if (blue === void 0) { blue = 0; }
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+    }
+    Colour.prototype.set = function (colour) {
+        this.red = colour.red;
+        this.green = colour.green;
+        this.blue = colour.blue;
+    };
+    Colour.prototype.fromRgbString = function (rgbString) {
+        var values = rgbString.split(',').map(function (el) { return Math.min(parseInt(el.trim()), 255); });
+        this.red = values[0];
+        this.green = values[1];
+        this.blue = values[2];
+        this.setSliders();
+    };
+    Colour.prototype.toRgbString = function () {
+        return this.red + ", " + this.green + ", " + this.blue;
+    };
+    Colour.prototype.fromHexString = function (hexString) {
+        if (hexString.length == 4) {
+            this.red = hexToInt(hexString.slice(1, 2));
+            this.green = hexToInt(hexString.slice(2, 3));
+            this.blue = hexToInt(hexString.slice(3, 4));
+        }
+        else {
+            this.red = hexToInt(hexString.slice(1, 3));
+            this.green = hexToInt(hexString.slice(3, 5));
+            this.blue = hexToInt(hexString.slice(5, 7));
+        }
+        this.setSliders();
+    };
+    Colour.prototype.toHexString = function () {
+        return "#" + intToHex(this.red) + intToHex(this.green) + intToHex(this.blue);
+    };
+    Colour.prototype.setSliders = function () {
+        redSlider.value = this.red.toString();
+        greenSlider.value = this.green.toString();
+        blueSlider.value = this.blue.toString();
+        setInputs(this);
+    };
+    Colour.getRandom = function () {
+        return new Colour(randomColour(), randomColour(), randomColour());
+    };
+    return Colour;
+}());
+var Modes;
+(function (Modes) {
+    Modes[Modes["Foreground"] = 0] = "Foreground";
+    Modes[Modes["Background"] = 1] = "Background";
+})(Modes || (Modes = {}));
+var mode = Modes.Foreground;
+// Random Number Generator
+var randomIntBetween = function (min, max) { return Math.floor((Math.random() * (max - min + 1))); };
+var randomColour = function () { return randomIntBetween(0, 255); };
+// RGB To Hex
+var intToHex = function (int) {
+    int = Math.min(int, 255);
+    var out = int.toString(16);
+    if (out.length == 1) {
+        out = '0' + out;
+    }
+    return out.toUpperCase();
+};
+var hexToInt = function (hex) { return Math.min(parseInt(hex, 16), 255); };
+// Toggle Mode
+var toggleMode = function () {
+    if (mode == Modes.Foreground) {
+        mode = Modes.Background;
+        $('#title').innerText = "Background";
+        bgColour.setSliders();
+        updateColour();
+    }
+    else {
+        mode = Modes.Foreground;
+        $('#title').innerText = "Foreground";
+        fgColour.setSliders();
+        updateColour();
+    }
+};
+// Get Input Colour
+var getInputColour = function () {
+    var red = parseInt(redSlider.value);
+    var green = parseInt(greenSlider.value);
+    var blue = parseInt(blueSlider.value);
+    return new Colour(red, green, blue);
+};
+// Set Current Colour
+var setCurrentColour = function (colour) {
+    // Update Box Colour
+    var currentColour = (mode == Modes.Foreground) ? fgColour : bgColour;
+    var box = (mode == Modes.Foreground) ? colourBox : background;
+    // Update Current Colour 
+    currentColour.set(colour);
+    // Set Colour Visually
+    box.style.backgroundColor = colour.toHexString();
+};
+// Set Inputs
+var setInputs = function (colour) {
+    rgbInput.value = colour.toRgbString();
+    hexInput.value = colour.toHexString();
+};
+// Handle Rgb Change
+var handleRgbChange = function () {
+    var currentColour = (mode == Modes.Foreground) ? fgColour : bgColour;
+    currentColour.fromRgbString(rgbInput.value);
+    setCurrentColour(currentColour);
+};
+// Handle Hex Change
+var handleHexChange = function () {
+    var currentColour = (mode == Modes.Foreground) ? fgColour : bgColour;
+    currentColour.fromHexString(hexInput.value);
+    setCurrentColour(currentColour);
+};
+// Handle Slider Scroll
+var handleSliderScroll = function (e) {
+    var sliderEl = e.target;
+    var currentColour = (mode == Modes.Foreground) ? fgColour : bgColour;
+    var delta = 1;
+    if (e.shiftKey) {
+        delta *= 5;
+    }
+    if (e.ctrlKey) {
+        delta *= 3;
+        // Prevent Page Zoom
+        e.preventDefault();
+    }
+    if (e.deltaY < 0) {
+        // Scrolling up
+        currentColour[sliderEl.id] += delta;
+    }
+    else {
+        // Scrolling down
+        currentColour[sliderEl.id] -= delta;
+    }
+    currentColour.setSliders();
+    updateColour();
+};
+// On load
+var bgColour = Colour.getRandom();
+background.style.backgroundColor = bgColour.toHexString();
+var fgColour = Colour.getRandom();
+colourBox.style.backgroundColor = fgColour.toHexString();
+fgColour.setSliders();
+// Update Colour
+var updateColour = function () {
+    var inputColour = getInputColour();
+    setCurrentColour(inputColour);
+    setInputs(inputColour);
+};
+// Add Event Listeners
+updateColour();
+redSlider.addEventListener('input', updateColour);
+redSlider.addEventListener('wheel', handleSliderScroll);
+greenSlider.addEventListener('input', updateColour);
+greenSlider.addEventListener('wheel', handleSliderScroll);
+blueSlider.addEventListener('input', updateColour);
+blueSlider.addEventListener('wheel', handleSliderScroll);
+rgbInput.addEventListener('change', handleRgbChange);
+hexInput.addEventListener('change', handleHexChange);
---
+++ js/index.ts
@@ -0,0 +1,243 @@
+const $ = <T = HTMLElement>(
+	query: string
+) => document.querySelector(query) as unknown as T
+
+// Get Elements
+
+const redSlider = $<HTMLInputElement>('#red')
+const greenSlider = $<HTMLInputElement>('#green')
+const blueSlider = $<HTMLInputElement>('#blue')
+
+const rgbInput = $<HTMLInputElement>('#rgb')
+const hexInput = $<HTMLInputElement>('#hex')
+
+const background = document.body
+const colourBox = $<HTMLDivElement>('#colour-box')
+
+class Colour {
+	constructor(
+		public red = 0,
+		public green = 0,
+		public blue = 0
+	) {}
+
+	set(colour: Colour) {
+		this.red = colour.red
+		this.green = colour.green
+		this.blue = colour.blue
+	}
+
+	fromRgbString(rgbString: string) {
+		const values = rgbString.split(',').map(el => Math.min(parseInt(el.trim()), 255))
+
+		this.red = values[0]
+		this.green = values[1]
+		this.blue = values[2]
+
+		this.setSliders()
+	}
+
+	toRgbString() {
+		return `${ this.red }, ${ this.green }, ${ this.blue }`
+	}
+
+	fromHexString(hexString: string) {
+		if (hexString.length == 4) {
+			this.red = hexToInt(hexString.slice(1, 2))
+			this.green = hexToInt(hexString.slice(2, 3))
+			this.blue = hexToInt(hexString.slice(3, 4))
+		} else {
+			this.red = hexToInt(hexString.slice(1, 3))
+			this.green = hexToInt(hexString.slice(3, 5))
+			this.blue = hexToInt(hexString.slice(5, 7))
+		}
+
+		this.setSliders()
+	}
+
+	toHexString() {
+		return `#${ intToHex(this.red) }${ intToHex(this.green) }${ intToHex(this.blue) }`
+	}
+
+	setSliders() {
+		redSlider.value = this.red.toString()
+		greenSlider.value = this.green.toString()
+		blueSlider.value = this.blue.toString()
+
+		setInputs(this)
+	}
+
+	static getRandom() {
+		return new Colour(randomColour(), randomColour(), randomColour())
+	}
+}
+
+enum Modes {
+	Foreground = 0,
+	Background = 1
+}
+
+let mode = Modes.Foreground
+
+// Random Number Generator
+
+const randomIntBetween = (
+	min: number,
+	max: number
+) => Math.floor(( Math.random() * (max - min + 1)))
+
+const randomColour = () => randomIntBetween(0, 255)
+
+// RGB To Hex
+
+const intToHex = (int: number) => {
+	int = Math.min(int, 255)
+
+	let out = int.toString(16)
+
+	if (out.length == 1) {
+		out = '0' + out
+	}
+
+	return out.toUpperCase()
+}
+
+const hexToInt = (hex: string) => Math.min(parseInt(hex, 16), 255)
+
+// Toggle Mode
+
+const toggleMode = () => {
+	if (mode == Modes.Foreground) {
+		mode = Modes.Background
+		$<HTMLHeadingElement>('#title').innerText = `Background`
+
+		bgColour.setSliders()
+		updateColour()
+	} else {
+		mode = Modes.Foreground
+		$<HTMLHeadingElement>('#title').innerText = `Foreground`
+
+		fgColour.setSliders()
+		updateColour()
+	}
+}
+
+// Get Input Colour
+
+const getInputColour = () => {
+	const red = parseInt(redSlider.value)
+	const green = parseInt(greenSlider.value)
+	const blue = parseInt(blueSlider.value)
+
+	return new Colour(red, green, blue)
+}
+
+// Set Current Colour
+
+const setCurrentColour = (colour: Colour) => {
+	// Update Box Colour
+
+	let currentColour = (mode == Modes.Foreground) ? fgColour : bgColour
+	const box = (mode == Modes.Foreground) ? colourBox : background
+
+	// Update Current Colour 
+
+	currentColour.set(colour)
+
+	// Set Colour Visually
+
+	box.style.backgroundColor = colour.toHexString()
+}
+
+// Set Inputs
+
+const setInputs = (colour: Colour) => {
+	rgbInput.value = colour.toRgbString()
+	hexInput.value = colour.toHexString()
+}
+
+// Handle Rgb Change
+
+const handleRgbChange = () => {
+	const currentColour = (mode == Modes.Foreground) ? fgColour : bgColour
+	
+	currentColour.fromRgbString(rgbInput.value)
+	setCurrentColour(currentColour)
+}
+
+// Handle Hex Change
+
+const handleHexChange = () => {
+	const currentColour = (mode == Modes.Foreground) ? fgColour : bgColour
+	
+	currentColour.fromHexString(hexInput.value)
+	setCurrentColour(currentColour)
+}
+
+// Handle Slider Scroll
+
+const handleSliderScroll = (
+	e: WheelEvent
+) => {
+	const sliderEl = e.target as HTMLInputElement
+	const currentColour = (mode == Modes.Foreground) ? fgColour : bgColour
+
+	let delta = 1
+
+	if (e.shiftKey) {
+		delta *= 5
+	}
+
+	if (e.ctrlKey) {
+		delta *= 3
+
+		// Prevent Page Zoom
+
+		e.preventDefault()
+	}
+
+	if (e.deltaY < 0) {
+		// Scrolling up
+
+		currentColour[sliderEl.id] += delta
+	} else {
+		// Scrolling down
+
+		currentColour[sliderEl.id] -= delta
+	}
+
+	currentColour.setSliders()
+	updateColour()
+}
+
+// On load
+
+let bgColour = Colour.getRandom()
+background.style.backgroundColor = bgColour.toHexString()
+
+
+let fgColour = Colour.getRandom()
+colourBox.style.backgroundColor = fgColour.toHexString()
+fgColour.setSliders()
+
+// Update Colour
+
+const updateColour = () => {
+	const inputColour = getInputColour()
+	setCurrentColour(inputColour)
+	setInputs(inputColour)
+}
+
+// Add Event Listeners
+
+updateColour()
+
+redSlider.addEventListener('input', updateColour)
+redSlider.addEventListener('wheel', handleSliderScroll)
+greenSlider.addEventListener('input', updateColour)
+greenSlider.addEventListener('wheel', handleSliderScroll)
+blueSlider.addEventListener('input', updateColour)
+blueSlider.addEventListener('wheel', handleSliderScroll)
+
+rgbInput.addEventListener('change', handleRgbChange)
+hexInput.addEventListener('change', handleHexChange)
